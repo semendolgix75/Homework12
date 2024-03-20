b30f884165252c998911b0555d6068bfd62499aa
Semen <semen.dolgix@yandex.ru>
2024-03-20 19:37:46 +0500

    Correct homework

--- src/Main.java
+++ src/Main.java
@@ -20,53 +20,5 @@ public class Main {
     }
 }
 
-//Создайте класс Book, который будет содержать в себе данные о названии,
-// авторе и годе публикации книги.
-class Book {
-    private String title;
-    private Author author;
-    private int yearPublic;
-    //Напишите конструкторы для обоих классов, заполняющие все поля.
-    public Book(String title, Author autor, int yearPublic) {
-        this.title = title;
-        this.author = autor;
-        this.yearPublic = yearPublic;
-    }
-    //    Создайте геттеры для всех полей автора и всех полей книги.
-    public String getTitle() {
-        return title;
-    }
-    public int getYearPublic() {
-        return yearPublic;
-    }
-    //Создайте сеттер для поля «Год публикации» у книги.
-    public void setYearPublic(int yearPublic) {
-        this.yearPublic = yearPublic;
-    }
 
-}
-//Создайте класс Author, который содержит в себе данные об имени и фамилии автора.
-class Author {
-    private String name;
-    private String surname;
 
-    //Напишите конструкторы для обоих классов, заполняющие все поля.
-    public Author() {
-        this.name = name;
-        this.surname = surname;
-    }
-
-    public Author(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
-    }
-
-    //    Создайте геттеры для всех полей автора и всех полей книги.
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-}
